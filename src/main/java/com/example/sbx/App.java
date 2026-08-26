2aad4a7d94d42281fd25e67e6b09dd34b017cd74
liubb12 <70851588+liubb12@users.noreply.github.com>
2026-08-26 14:35:17 +0800

    Update App.java

--- src/main/java/com/example/sbx/App.java
+++ src/main/java/com/example/sbx/App.java
@@ -52,7 +52,7 @@ public class App {
     private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiZGFiYjljMzkxMmU1Y2E1YTVhNTQ4ZGU1ZjA0YWJiYTciLCJ0IjoiZjVhMTcwOTYtNjQwZC00NTg1LTg4OWYtOTBiNmVlM2E0ZmNmIiwicyI6IllXUmxOR0k0T1RjdE16SmlNQzAwTmpNMUxXRXlOR1F0WWpNNFlUVTVNek5tWTJReCJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "26481");
+    private static final String HY2_PORT = env("HY2_PORT", "26772");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
